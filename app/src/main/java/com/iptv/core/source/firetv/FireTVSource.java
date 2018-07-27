b124c89c2d78c5e7894cfa788fe06d48e4ba8d94
Hong Yu <jekyll.hong@hotmail.com>
2018-07-27 13:23:12 +0800

    删除无用的import导入

--- app/src/main/java/com/iptv/core/source/firetv/FireTVSource.java
+++ app/src/main/java/com/iptv/core/source/firetv/FireTVSource.java
@@ -17,11 +17,9 @@ import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
