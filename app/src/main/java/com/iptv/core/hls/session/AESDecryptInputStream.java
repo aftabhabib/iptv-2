abab60adb83a678c8640e3e0f9f0296e60f1476f
Hong Yu <jekyll.hong@hotmail.com>
2018-08-03 17:16:50 +0800

    修改为初始化失败时跑出状态异常

--- app/src/main/java/com/iptv/core/hls/session/AESDecryptInputStream.java
+++ app/src/main/java/com/iptv/core/hls/session/AESDecryptInputStream.java
@@ -2,60 +2,41 @@ package com.iptv.core.hls.session;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
+import java.security.GeneralSecurityException;
 import java.security.Key;
-import java.security.NoSuchAlgorithmException;
 import java.security.spec.AlgorithmParameterSpec;
 
 import javax.crypto.Cipher;
 import javax.crypto.CipherInputStream;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 class AESDecryptInputStream extends InputStream {
     private CipherInputStream mCipherInput;
 
-    public AESDecryptInputStream(InputStream input, byte[] key, byte[] initVector) {
+    public AESDecryptInputStream(InputStream input, byte[] key, byte[] iv) {
         try {
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding");
 
             Key cipherKey = new SecretKeySpec(key, "AES");
-            AlgorithmParameterSpec cipherIV = new IvParameterSpec(initVector);
+            AlgorithmParameterSpec cipherIV = new IvParameterSpec(iv);
 
             cipher.init(Cipher.DECRYPT_MODE, cipherKey, cipherIV);
 
             mCipherInput = new CipherInputStream(input, cipher);
         }
-        catch (NoSuchAlgorithmException e) {
-            //ignore
-        }
-        catch (NoSuchPaddingException e) {
-            //ignore
-        }
-        catch (InvalidAlgorithmParameterException e) {
-            //ignore
-        }
-        catch (InvalidKeyException e) {
-            //ignore
+        catch (GeneralSecurityException e) {
+            throw new IllegalStateException("Cipher init fail");
         }
     }
 
     @Override
     public int read() throws IOException {
-        if (mCipherInput == null) {
-            throw new IllegalStateException("can not decrypt");
-        }
-
         return mCipherInput.read();
     }
 
     @Override
     public void close() throws IOException {
-        if (mCipherInput != null) {
-            mCipherInput.close();
-            mCipherInput = null;
-        }
+        mCipherInput.close();
     }
 }
