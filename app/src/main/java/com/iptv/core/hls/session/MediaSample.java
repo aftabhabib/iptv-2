fc1c39b6bb064908f0500975903819c5178e5425
Hong Yu <jekyll.hong@hotmail.com>
2018-08-06 15:53:23 +0800

    新增一个MediaSample类，用于负载音视频、字幕的数据单元

---
+++ app/src/main/java/com/iptv/core/hls/session/MediaSample.java
@@ -0,0 +1,31 @@
+package com.iptv.core.hls.session;
+
+import java.nio.ByteBuffer;
+
+public abstract class MediaSample {
+    private static final int MAX_SAMPLE_SIZE = 128 * 1000;
+
+    private int mCodecType;
+
+    private long mPresentationTimestamp;
+    private long mDecodingTimestamp;
+
+    private ByteBuffer mDataBuffer;
+
+    public MediaSample(int codecType, long pts, long dts) {
+        mCodecType = codecType;
+
+        mPresentationTimestamp = pts;
+        mDecodingTimestamp = dts;
+
+        mDataBuffer.allocateDirect(MAX_SAMPLE_SIZE);
+    }
+
+    public void write(byte[] buf) {
+        mDataBuffer.put(buf, 0, buf.length);
+    }
+
+    public void write(byte[] buf, int offset, int size) {
+        mDataBuffer.put(buf, offset, size);
+    }
+}
