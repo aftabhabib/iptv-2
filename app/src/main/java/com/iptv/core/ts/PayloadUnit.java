0f66ad8233b0fe8173e478776982a21d8abddf8b
Hong Yu <jekyll.hong@hotmail.com>
2018-08-09 13:37:27 +0800

    新增PayloadUnit类

---
+++ app/src/main/java/com/iptv/core/ts/PayloadUnit.java
@@ -0,0 +1,60 @@
+package com.iptv.core.ts;
+
+import java.nio.ByteBuffer;
+
+class PayloadUnit {
+    private ByteBuffer mBuffer;
+
+    private int mCounter = -1;
+
+    public PayloadUnit(int capacity) {
+        mBuffer = ByteBuffer.allocateDirect(capacity);
+    }
+
+    /**
+     * （包序）计数是否连续
+     */
+    public boolean isDiscontinuous(int counter) {
+        if (mCounter == -1) {
+            return false;
+        }
+        else {
+            /**
+             * counter is 4 bits field
+             */
+            return ((mCounter + 1) & 0x0f) == counter;
+        }
+    }
+
+    /**
+     * 写入数据，同时更新（包序）计数
+     */
+    public void write(byte[] data, int counter) {
+        mBuffer.put(data);
+
+        mCounter = counter;
+    }
+
+    /**
+     * 重置
+     */
+    public void reset() {
+        mBuffer.clear();
+
+        mCounter = -1;
+    }
+
+    /**
+     * 获取负载单元的数据
+     */
+    public byte[] data() {
+        return mBuffer.array();
+    }
+
+    /**
+     * 清空数据
+     */
+    public void clear() {
+        mBuffer.clear();
+    }
+}
