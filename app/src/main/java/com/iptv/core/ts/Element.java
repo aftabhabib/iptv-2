b8f0f3311399e9afa96f659a9c5dad1291b5d946
Hong Yu <jekyll.hong@hotmail.com>
2018-08-13 17:25:25 +0800

    新增Element类

---
+++ app/src/main/java/com/iptv/core/ts/Element.java
@@ -0,0 +1,13 @@
+package com.iptv.core.ts;
+
+final class Element {
+    private int mType;
+
+    public Element(int type) {
+        mType = type;
+    }
+
+    public void putPESPacket(PESPacket pesPacket) {
+        //
+    }
+}
