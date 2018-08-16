83588b5f8f6f38e7b4a451bbd91fe8231030688c
Hong Yu <jekyll.hong@hotmail.com>
2018-08-16 16:46:49 +0800

    Source接口改名为Resource，同时增加Listener；Plugin接口加注释

--- app/src/main/java/com/iptv/core/source/Plugin.java
+++ app/src/main/java/com/iptv/core/source/Plugin.java
@@ -1,16 +1,21 @@
 package com.iptv.core.source;
 
+/**
+ * 插件
+ */
 public interface Plugin {
     /**
      * 获取名称
      */
     String getName();
+
     /**
      * 是否支持
      */
     boolean isSupported(String url);
+
     /**
-     * 解码
+     * 翻译
      */
-    String decode(String url);
+    String translate(String url);
 }
---
+++ app/src/main/java/com/iptv/core/source/Resource.java
@@ -0,0 +1,55 @@
+package com.iptv.core.source;
+
+import com.iptv.core.channel.ChannelGroup;
+
+import java.util.Map;
+
+/**
+ * 资源
+ */
+public interface Resource {
+    /**
+     * 事件回调
+     */
+    interface Listener {
+        /**
+         * 加载频道表响应
+         */
+        void onLoadChannelTable(ChannelGroup[] groups);
+
+        /**
+         * 解码url响应
+         */
+        void onDecodeSource(String url, Map<String, String> properties);
+
+        /**
+         * 出错
+         */
+        void onError(String error);
+    }
+
+    /**
+     * 注册事件回调
+     */
+    void setListener(Listener listener);
+
+    /**
+     * 获取名称
+     */
+    String getName();
+
+    /**
+     * 加载频道表
+     */
+    void loadChannelTable();
+
+    /**
+     * 解码数据源
+     */
+    void decodeSource(String source);
+
+    /**
+     * 释放
+     */
+    void release();
+}
