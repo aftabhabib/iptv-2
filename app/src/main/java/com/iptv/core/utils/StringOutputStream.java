7c74f80c4cec6c4b62b70cfd72b84caa51092ac4
Hong Yu <jekyll.hong@hotmail.com>
2018-08-17 14:56:04 +0800

    新增StringOutputStream类

---
+++ app/src/main/java/com/iptv/core/utils/StringOutputStream.java
@@ -0,0 +1,45 @@
+package com.iptv.core.utils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+
+public class StringOutputStream extends OutputStream {
+    private ByteArrayOutputStream mBuffer;
+
+    /**
+     * 构造函数
+     */
+    public StringOutputStream() {
+        mBuffer = new ByteArrayOutputStream();
+    }
+
+    @Override
+    public void write(int value) throws IOException {
+        mBuffer.write(value);
+    }
+
+    /**
+     * 转换为指定编码格式的字符串
+     */
+    public String toString(String charset) throws UnsupportedEncodingException {
+        return new String(mBuffer.toByteArray(), charset);
+    }
+
+    @Override
+    public String toString() {
+        String ret = "";
+
+        try {
+            ret = toString("UTF-8");
+        }
+        catch (UnsupportedEncodingException e) {
+            /**
+             * ignore
+             */
+        }
+
+        return ret;
+    }
+}
