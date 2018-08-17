a5b258f8b5f089ee4f51d27048882dd5d467176f
Hong Yu <jekyll.hong@hotmail.com>
2018-08-17 16:07:40 +0800

    因为星火New直播版本更新的很快，所以resource模块的某个具体实现留到最后吧。工厂模式保留

--- app/src/main/java/com/iptv/core/resource/ResourceFactory.java
+++ app/src/main/java/com/iptv/core/resource/ResourceFactory.java
@@ -2,27 +2,15 @@ package com.iptv.core.resource;
 
 import android.content.Context;
 
-import com.iptv.core.resource.firetv.FireTVResource;
-import com.iptv.core.resource.supertv.SuperTVSource;
-
 /**
  * 资源工厂
  */
 public final class ResourceFactory {
-    public static final int RESOURCE_TYPE_FIRETV = 0;
-    public static final int RESOURCE_TYPE_SUPERTV = 1;
-
     /**
      * 创建资源
      */
     public static Resource createResource(int type, Context context) {
         switch (type) {
-            case RESOURCE_TYPE_FIRETV: {
-                return new FireTVResource(context);
-            }
-            case RESOURCE_TYPE_SUPERTV: {
-                return new SuperTVSource(context);
-            }
             default: {
                 return null;
             }
