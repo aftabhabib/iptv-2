15bbc4be6d5a374eda2a4623ac437f1ea98b5595
Hong Yu <jekyll.hong@hotmail.com>
2018-08-17 11:13:22 +0800

    修改TVListParser的实现

--- app/src/main/java/com/iptv/core/source/firetv/TVListParser.java
+++ app/src/main/java/com/iptv/core/source/firetv/TVListParser.java
@@ -8,14 +8,15 @@ import com.iptv.core.channel.ChannelGroup;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * 解析tvlist.xml
+ */
 final class TVListParser {
     private static final String XML_TAG_TVLISTS = "tvlists";
     private static final String XML_TAG_TVLIST = "tvlist";
@@ -24,150 +25,174 @@ final class TVListParser {
 
     private static final String XML_ATTR_ID = "id";
 
-    public static List<Channel> parse(File file) {
-        List<Channel> channels;
+    private XmlPullParser mParser;
 
-        FileInputStream input = null;
-        try {
-            input = new FileInputStream(file);
-            channels = parseXml(input);
-        }
-        catch (IOException e) {
-            channels = null;
-        }
-        finally {
-            if (input != null) {
-                try {
-                    input.close();
-                }
-                catch (IOException e) {
-                    //ignore
-                }
-            }
-        }
-
-        return channels;
+    /**
+     * 构造函数
+     */
+    public TVListParser() {
+        mParser = Xml.newPullParser();
     }
 
-    private static List<Channel> parseXml(InputStream input) throws IOException {
-        try {
-            XmlPullParser parser = Xml.newPullParser();
-            parser.setInput(input, "utf-8");
+    /**
+     * 读取频道列表
+     */
+    public List<Channel> readChannelList(InputStream input) throws IOException {
+        List<Channel> channelList = new LinkedList<Channel>();
 
-            parser.nextTag();
-            return readChannels(parser);
+        try {
+            mParser.setInput(input, "utf-8");
+
+            /**
+             * xml档案中只关注<tvlists></tvlists>标签
+             */
+            while (mParser.next() != XmlPullParser.END_DOCUMENT) {
+                if (mParser.getEventType() == XmlPullParser.START_TAG) {
+                    String tagName = mParser.getName();
+                    if (tagName.equals(XML_TAG_TVLISTS)) {
+                        parseTVLists(channelList);
+                    }
+                    else {
+                        skipTag();
+                    }
+                }
+            }
         }
         catch (XmlPullParserException e) {
-            throw new IOException("parse fail");
-        }
-    }
-
-    private static List<Channel> readChannels(XmlPullParser parser) throws XmlPullParserException, IOException {
-        parser.require(XmlPullParser.START_TAG, null, XML_TAG_TVLISTS);
-
-        List<Channel> channels = new LinkedList<Channel>();
-
-        while (parser.next() != XmlPullParser.END_TAG) {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                continue;
-            }
-
-            String tagName = parser.getName();
-            if (tagName.equals(XML_TAG_TVLIST)) {
-                Channel channel = readChannel(parser);
-                channels.add(channel);
-            }
-            else {
-                skip(parser);
-            }
+            /**
+             * ignore
+             */
         }
 
-        return channels;
+        return channelList;
     }
 
-    private static Channel readChannel(XmlPullParser parser) throws XmlPullParserException, IOException {
-        parser.require(XmlPullParser.START_TAG, null, XML_TAG_TVLIST);
+    /**
+     * 解析<tvlists></tvlists>标签里的内容
+     */
+    private void parseTVLists(List<Channel> channelList) throws XmlPullParserException, IOException {
+        mParser.require(XmlPullParser.START_TAG, null, XML_TAG_TVLISTS);
 
-        Channel channel = new Channel();
+        /**
+         * 次一级标签里只关注<tvlist></tvlist>
+         */
+        while (mParser.next() != XmlPullParser.END_TAG) {
+            if (mParser.getEventType() == XmlPullParser.START_TAG) {
+                String tagName = mParser.getName();
+                if (tagName.equals(XML_TAG_TVLIST)) {
+                    Channel.Builder builder = new Channel.Builder();
 
-        String value = parser.getAttributeValue(null, XML_ATTR_ID);
-        if (value.contains("|")) {
-            String[] results = value.split("|");
+                    parseTVList(builder);
+
+                    channelList.add(builder.build());
+                }
+                else {
+                    skipTag();
+                }
+            }
+        }
+
+        mParser.require(XmlPullParser.END_TAG, null, XML_TAG_TVLISTS);
+    }
+
+    /**
+     * 解析<tvlist></tvlist>标签里的内容
+     */
+    private void parseTVList(Channel.Builder builder) throws XmlPullParserException, IOException {
+        mParser.require(XmlPullParser.START_TAG, null, XML_TAG_TVLIST);
+
+        /**
+         * 分组Id是标签的属性
+         */
+        String groupId = mParser.getAttributeValue(null, XML_ATTR_ID);
+        if (groupId.contains("|")) {
+            String[] results = groupId.split("|");
 
             for (int i = 0; i < results.length; i++) {
-                channel.addGroupId(results[i]);
+                builder.addGroupId(results[i]);
             }
         }
         else {
-            channel.addGroupId(value);
+            builder.addGroupId(groupId);
         }
 
-        while (parser.next() != XmlPullParser.END_TAG) {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                continue;
-            }
-
-            String tagName = parser.getName();
-            if (tagName.equals(XML_TAG_TVNAME)) {
-                String text = readName(parser);
-
-                channel.setName(text);
-            }
-            else if (tagName.equals(XML_TAG_TVADDR)) {
-                String text = readSource(parser);
-
-                if (text.contains("|")) {
-                    String[] results = text.split("|");
-
-                    for (int i = 0; i < results.length; i++) {
-                        channel.addSource(results[i]);
-                    }
+        /**
+         * 次一级标签里只关注<tvname></tvname>和<tvaddr></tvaddr>
+         */
+        while (mParser.next() != XmlPullParser.END_TAG) {
+            if (mParser.getEventType() == XmlPullParser.START_TAG) {
+                String tagName = mParser.getName();
+                if (tagName.equals(XML_TAG_TVNAME)) {
+                    parseTVName(builder);
+                }
+                else if (tagName.equals(XML_TAG_TVADDR)) {
+                    parseTVAddress(builder);
                 }
                 else {
-                    channel.addSource(text);
+                    skipTag();
+                }
+            }
+        }
+
+        mParser.require(XmlPullParser.END_TAG, null, XML_TAG_TVLIST);
+    }
+
+    /**
+     * 解析<tvname></tvname>标签的内容
+     */
+    private void parseTVName(Channel.Builder builder) throws XmlPullParserException, IOException {
+        mParser.require(XmlPullParser.START_TAG, null, XML_TAG_TVNAME);
+
+        /**
+         * 标签的内容应该是文本
+         */
+        if (mParser.next() == XmlPullParser.TEXT) {
+            String name = mParser.getText();
+            builder.setName(name);
+
+            mParser.nextTag();
+        }
+
+        mParser.require(XmlPullParser.END_TAG, null, XML_TAG_TVNAME);
+    }
+
+    /**
+     * 解析<tvaddr></tvaddr>标签的内容
+     */
+    private void parseTVAddress(Channel.Builder builder) throws XmlPullParserException, IOException {
+        mParser.require(XmlPullParser.START_TAG, null, XML_TAG_TVADDR);
+
+        /**
+         * 标签的内容应该是文本
+         */
+        if (mParser.next() == XmlPullParser.TEXT) {
+            String address = mParser.getText();
+
+            if (address.contains("|")) {
+                String[] results = address.split("|");
+
+                for (int i = 0; i < results.length; i++) {
+                    builder.addSource(results[i]);
                 }
             }
             else {
-                skip(parser);
+                builder.addSource(address);
             }
+
+            mParser.nextTag();
         }
 
-        return channel;
+        mParser.require(XmlPullParser.END_TAG, null, XML_TAG_TVADDR);
     }
 
-    private static String readName(XmlPullParser parser) throws IOException, XmlPullParserException {
-        parser.require(XmlPullParser.START_TAG, null, XML_TAG_TVNAME);
-
-        String name = "";
-        if (parser.next() == XmlPullParser.TEXT) {
-            name = parser.getText().trim();
-            parser.nextTag();
-        }
-
-        parser.require(XmlPullParser.END_TAG, null, XML_TAG_TVNAME);
-
-        return name;
-    }
-
-    private static String readSource(XmlPullParser parser) throws IOException, XmlPullParserException {
-        parser.require(XmlPullParser.START_TAG, null, XML_TAG_TVADDR);
-
-        String source = "";
-        if (parser.next() == XmlPullParser.TEXT) {
-            source = parser.getText().trim();
-            parser.nextTag();
-        }
-
-        parser.require(XmlPullParser.END_TAG, null, XML_TAG_TVADDR);
-
-        return source;
-    }
-
-    private static void skip(XmlPullParser parser) throws IOException, XmlPullParserException {
+    /**
+     * 跳过标签（以及里面的内容）
+     */
+    private void skipTag() throws XmlPullParserException, IOException {
         int depth = 1;
 
         while (depth != 0) {
-            switch (parser.next()) {
+            switch (mParser.next()) {
                 case XmlPullParser.END_TAG: {
                     depth--;
                     break;
@@ -183,63 +208,69 @@ final class TVListParser {
         }
     }
 
-    public static List<ChannelGroup.GroupInfo> getGroupInfoList() {
-        final String[] GROUP_NAME = {
-                "央视频道",
-                "卫视频道",
-                "高清频道",
-                "数字频道",
-                "体育频道",
-                "新闻频道",
-                "影视频道",
-                "少儿频道",
-                "网络频道1",
-                "网络频道2",
-                "港台频道",
-                "海外频道",
-                "测试频道",
-                "北京",
-                "上海",
-                "天津",
-                "重庆",
-                "黑龙江",
-                "吉林",
-                "辽宁",
-                "新疆",
-                "甘肃",
-                "宁夏",
-                "青海",
-                "陕西",
-                "山西",
-                "河南",
-                "河北",
-                "安徽",
-                "山东",
-                "江苏",
-                "浙江",
-                "福建",
-                "广东",
-                "广西",
-                "云南",
-                "湖南",
-                "湖北",
-                "江西",
-                "海南",
-                "四川",
-                "贵州",
-                "西藏",
-                "内蒙古",
-                "自定义频道",
-                "已收藏频道",
-                "临时频道"
-        };
+    /**
+     * 分组信息
+     */
+    private static final String[] GROUP_NAME = {
+            "央视频道",
+            "卫视频道",
+            "高清频道",
+            "数字频道",
+            "体育频道",
+            "新闻频道",
+            "影视频道",
+            "少儿频道",
+            "网络频道1",
+            "网络频道2",
+            "港台频道",
+            "海外频道",
+            "测试频道",
+            "北京",
+            "上海",
+            "天津",
+            "重庆",
+            "黑龙江",
+            "吉林",
+            "辽宁",
+            "新疆",
+            "甘肃",
+            "宁夏",
+            "青海",
+            "陕西",
+            "山西",
+            "河南",
+            "河北",
+            "安徽",
+            "山东",
+            "江苏",
+            "浙江",
+            "福建",
+            "广东",
+            "广西",
+            "云南",
+            "湖南",
+            "湖北",
+            "江西",
+            "海南",
+            "四川",
+            "贵州",
+            "西藏",
+            "内蒙古",
+            "自定义频道",
+            "已收藏频道",
+            "临时频道"
+    };
 
-        List<ChannelGroup.GroupInfo> groupInfoList = new ArrayList<ChannelGroup.GroupInfo>(GROUP_NAME.length);
+    /**
+     * 频道分组
+     */
+    public List<ChannelGroup> groupChannels(Channel[] channels) {
+        List<ChannelGroup> groupList = new ArrayList<ChannelGroup>(GROUP_NAME.length);
 
         for (int i = 0; i < GROUP_NAME.length; i++) {
-            groupInfoList.add(new ChannelGroup.GroupInfo(String.valueOf(i + 1), GROUP_NAME[i]));
+            groupList.add(ChannelGroup.create(GROUP_NAME[i], String.valueOf(i + 1), channels));
         }
 
-        return groupInfoList;
+        return groupList;
     }
 }
