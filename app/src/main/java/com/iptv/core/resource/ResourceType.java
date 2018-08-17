c87f7624effe1e1124487435c1f3cd2fcd8624d6
Hong Yu <jekyll.hong@hotmail.com>
2018-08-17 16:18:39 +0800

    新建ResourceType类，资源类型分为两种：来自app和来自网站

---
+++ app/src/main/java/com/iptv/core/resource/ResourceType.java
@@ -0,0 +1,25 @@
+package com.iptv.core.resource;
+
+/**
+ * 资源类型
+ */
+public final class ResourceType {
+    /**
+     * 应用
+     */
+    public static final int APP_FIRETV = 0;
+
+    /**
+     * 网站
+     */
+    public static final int WEBSITE_TEST = 100;
+
+    /**
+     * 构造函数（私有属性，不允许创建实例）
+     */
+    private ResourceType() {
+        /**
+         * nothing
+         */
+    }
+}
