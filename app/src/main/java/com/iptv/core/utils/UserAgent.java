34355971b3dac937bef574f0cdb91f26ed56a48d
Hong Yu <jekyll.hong@hotmail.com>
2018-08-21 16:09:51 +0800

    新建UserAgent类，专门管理各种UA

---
+++ app/src/main/java/com/iptv/core/utils/UserAgent.java
@@ -0,0 +1,14 @@
+package com.iptv.core.utils;
+
+public class UserAgent {
+    public static final String UA_WINDOWS = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Maxthon/4.9.3.1000 Chrome/39.0.2146.0 Safari/537.36";
+
+    /**
+     * 构造函数（私有属性，不允许创建实例）
+     */
+    private UserAgent() {
+        /**
+         * nothing
+         */
+    }
+}
