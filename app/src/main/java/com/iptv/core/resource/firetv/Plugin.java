561333260cd1cc53711ae110e51ddbd4686d75d5
Hong Yu <jekyll.hong@hotmail.com>
2018-08-22 15:49:14 +0800

    resource包下的Plugin接口和PluginManager类移至firetv包内，同时修改PluginManager的类属性

---
+++ app/src/main/java/com/iptv/core/resource/firetv/Plugin.java
@@ -0,0 +1,28 @@
+package com.iptv.core.resource.firetv;
+
+import java.util.Map;
+
+/**
+ * 插件
+ */
+public interface Plugin {
+    /**
+     * 获取名称
+     */
+    String getName();
+
+    /**
+     * 是否支持
+     */
+    boolean isSupported(String url);
+
+    /**
+     * 翻译
+     */
+    String translate(String url);
+
+    /**
+     * 获取属性
+     */
+    Map<String, String> getProperties();
+}
