17bf4c117180a3a7ea83668d0e9a605c475d9753
Hong Yu <jekyll.hong@hotmail.com>
2018-08-28 10:59:37 +0800

    IOUtils增加lines方法

--- app/src/main/java/com/iptv/core/utils/IOUtils.java
+++ app/src/main/java/com/iptv/core/utils/IOUtils.java
@@ -1,8 +1,12 @@
 package com.iptv.core.utils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.StringReader;
+import java.util.LinkedList;
+import java.util.List;
 
 public class IOUtils {
     /**
@@ -19,6 +23,45 @@ public class IOUtils {
 
             output.write(buf, 0, bytesRead);
         }
+    }
+
+    /**
+     * 文本内容以行为单位
+     */
+    public static String[] lines(String content) {
+        List<String> lineList = new LinkedList<String>();
+
+        BufferedReader reader = new BufferedReader(new StringReader(content));
+        try {
+            while (true) {
+                String line = reader.readLine();
+                if (line == null) {
+                    break;
+                }
+
+                lineList.add(line);
+            }
+        }
+        catch (IOException e) {
+            /**
+             * 出错，清除中间结果
+             */
+            if (!lineList.isEmpty()) {
+                lineList.clear();
+            }
+        }
+        finally {
+            try {
+                reader.close();
+            }
+            catch (IOException e) {
+                /**
+                 * ignore
+                 */
+            }
+        }
+
+        return lineList.isEmpty() ? null : lineList.toArray(new String[lineList.size()]);
     }
 
     /**
