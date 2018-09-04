95a51ddcf97a4c8ae08ba597fa458c695fb8e992
Hong Yu <jekyll.hong@hotmail.com>
2018-09-04 11:31:07 +0800

    HttpSource增加setDecryptCipher方法，用于PlaylistSource中segment加密的情况

--- app/src/main/java/com/iptv/core/player/source/HttpSource.java
+++ app/src/main/java/com/iptv/core/player/source/HttpSource.java
@@ -9,6 +9,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+
 import okhttp3.Response;
 
 public class HttpSource implements DataSource {
@@ -20,12 +23,21 @@ public class HttpSource implements DataSource {
     private long mContentSize = -1;
     private InputStream mContentInput = null;
 
+    private Cipher mCipher = null;
+
     /**
      * 构造函数
      */
     public HttpSource(String url, Map<String, String> properties) {
         mUrl = url;
         mProperties = properties;
+    }
+
+    /**
+     * 设置解密密钥
+     */
+    public void setDecryptCipher(Cipher cipher) {
+        mCipher = cipher;
     }
 
     /**
@@ -41,17 +53,24 @@ public class HttpSource implements DataSource {
                 String contentLength = response.header("Content-Length", "-1");
                 mContentSize = Long.parseLong(contentLength);
 
-                mContentInput = response.body().byteStream();
+                InputStream input = response.body().byteStream();
+                if (mCipher != null) {
+                    mContentInput = new CipherInputStream(input, mCipher);
+                }
+                else {
+                    mContentInput = input;
+                }
             }
             else {
-                Log.e(TAG, "connect fail, " + response.message());
+                Log.e(TAG, "GET " + mUrl + " fail, " + response.message());
+
+                response.close();
             }
         }
         catch (IOException e) {
-            Log.e(TAG, "connect error");
-        }
-        finally {
-            if (mContentInput == null) {
+            Log.e(TAG, "read " + mUrl + " error");
+
+            if (response != null) {
                 response.close();
             }
         }
