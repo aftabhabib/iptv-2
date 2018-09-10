a5f8b62c33066897245272e06fa07be21d736f57
Hong Yu <jekyll.hong@hotmail.com>
2018-09-10 09:24:40 +0800

    增加方法注释

--- app/src/main/java/com/iptv/core/hls/playlist/AttributeValue.java
+++ app/src/main/java/com/iptv/core/hls/playlist/AttributeValue.java
@@ -7,18 +7,21 @@ import java.math.BigInteger;
  */
 public class AttributeValue {
     /**
-     * an integer in base-10 arithmetic in the range from 0 to 2^64-1
+     * 读十进制的整数
      */
     public static int readDecimalInteger(String attributeValue) {
         return Integer.parseInt(attributeValue);
     }
 
+    /**
+     * 写十进制的整数
+     */
     public static String writeDecimalInteger(int content) {
         return String.valueOf(content);
     }
 
     /**
-     *
+     * 读十六进制的序列
      */
     public static byte[] readHexadecimalSequence(String attributeValue) {
         if (!attributeValue.startsWith("0x")
@@ -29,6 +32,9 @@ public class AttributeValue {
         return new BigInteger(attributeValue.substring(2), 16).toByteArray();
     }
 
+    /**
+     * 写十六进制的序列
+     */
     public static String writeHexadecimalSequence(byte[] content) {
         StringBuffer buffer = new StringBuffer("0x");
 
@@ -46,42 +52,72 @@ public class AttributeValue {
     }
 
     /**
-     * a floating-point number in decimal positional notation
+     * 读十进制的浮点数
      */
     public static float readDecimalFloatingPoint(String attributeValue) {
         return Float.parseFloat(attributeValue);
     }
 
+    /**
+     * 写十进制的浮点数
+     */
     public static String writeDecimalFloatingPoint(float content) {
         return String.valueOf(content);
     }
 
     /**
-     * a string of characters within a pair of double quotes
-     * MUST NOT appear: line feed (0xA), carriage return (0xD), or double quote (0x22)
+     * 读使用双引号括起来的字符串（字符串里不能有双引号和换行符）
      */
     public static String readQuotedString(String attributeValue) {
+        if (!attributeValue.startsWith("\"")
+                && !attributeValue.endsWith("\"")) {
+            throw new IllegalArgumentException("bad value");
+        }
+
         return attributeValue.substring(1, attributeValue.length() - 1);
     }
 
+    /**
+     * 写使用双引号括起来的字符串（字符串里不能有双引号和换行符）
+     */
     public static String writeQuotedString(String content) {
+        if (content.contains("\"")
+                || content.contains("\r")
+                || content.contains("\n")) {
+            throw new IllegalArgumentException("bad value");
+        }
+
         return "\"" + content + "\"";
     }
 
     /**
-     * an unquoted character string
-     * never contain double quotes ("), commas (,), or whitespace
+     * 读枚举字符串（字符串里不能有双引号、逗号和空格）
      */
     public static String readEnumeratedString(String attributeValue) {
+        if (attributeValue.contains("\"")
+                || attributeValue.contains(",")
+                || attributeValue.contains(" ")) {
+            throw new IllegalArgumentException("bad value");
+        }
+
         return attributeValue;
     }
 
+    /**
+     * 写枚举字符串（字符串里不能有双引号、逗号和空格）
+     */
     public static String writeEnumeratedString(String content) {
+        if (content.contains("\"")
+                || content.contains(",")
+                || content.contains(" ")) {
+            throw new IllegalArgumentException("bad value");
+        }
+
         return content;
     }
 
     /**
-     * two decimal-integers separated by the "x" character
+     * 读分辨率（使用“x”分隔的两个整数）
      */
     public static Resolution readDecimalResolution(String attributeValue) {
         if (!attributeValue.contains("x")) {
@@ -96,6 +132,9 @@ public class AttributeValue {
         return new Resolution(Integer.parseInt(result[0]), Integer.parseInt(result[1]));
     }
 
+    /**
+     * 写分辨率（使用“x”分隔的两个整数）
+     */
     public static String writeDecimalResolution(Resolution resolution) {
         return String.valueOf(resolution.getWidth())
                 + "x"
