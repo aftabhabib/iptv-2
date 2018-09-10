2c04356a0bf9ecca08b0c58168684c031a156b87
Hong Yu <jekyll.hong@hotmail.com>
2018-09-10 09:59:28 +0800

    Resolution整合进AttributeValue

--- app/src/main/java/com/iptv/core/hls/playlist/AttributeValue.java
+++ app/src/main/java/com/iptv/core/hls/playlist/AttributeValue.java
@@ -149,4 +149,34 @@ public class AttributeValue {
          * nothing
          */
     }
+
+    /**
+     * 视频图像分辨率
+     */
+    public static class Resolution {
+        private int mWidth;
+        private int mHeight;
+
+        /**
+         * 构造函数
+         */
+        public Resolution(int width, int height) {
+            mWidth = width;
+            mHeight = height;
+        }
+
+        /**
+         * 获取视频图像宽
+         */
+        public int getWidth() {
+            return mWidth;
+        }
+
+        /**
+         * 获取视频图像高
+         */
+        public int getHeight() {
+            return mHeight;
+        }
+    }
 }
