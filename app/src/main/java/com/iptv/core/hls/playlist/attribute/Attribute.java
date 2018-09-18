d02000ecb58feeb24a7c16b346717c2389b3e39d
Hong Yu <jekyll.hong@hotmail.com>
2018-09-18 15:24:48 +0800

    回退之前的Attribute类，重构。将AttributeName类合并入Attribute类。修改AttributeList类，不再关心value的类型，层次划分分明

---
+++ app/src/main/java/com/iptv/core/hls/playlist/attribute/Attribute.java
@@ -0,0 +1,201 @@
+package com.iptv.core.hls.playlist.attribute;
+
+import com.iptv.core.hls.exception.MalformedFormatException;
+import com.iptv.core.hls.playlist.datatype.HexadecimalSequence;
+import com.iptv.core.hls.playlist.datatype.QuotedString;
+import com.iptv.core.hls.playlist.datatype.Resolution;
+
+/**
+ * 属性
+ */
+public final class Attribute {
+    private String mName;
+    private String mValue;
+
+    /**
+     * 构造函数
+     */
+    private Attribute(String name, String value) {
+        mName = name;
+        mValue = value;
+    }
+
+    /**
+     * 获取属性名
+     */
+    public String getName() {
+        return mName;
+    }
+
+    /**
+     * 获取整型属性值
+     */
+    public int getIntegerValue() throws MalformedFormatException {
+        try {
+            return Integer.parseInt(mValue);
+        }
+        catch (NumberFormatException e) {
+            throw new MalformedFormatException("should be decimal integer");
+        }
+    }
+
+    /**
+     * 获取长整型属性值
+     */
+    public long getLongValue() throws MalformedFormatException {
+        try {
+            return Long.parseLong(mValue);
+        }
+        catch (NumberFormatException e) {
+            throw new MalformedFormatException("should be decimal integer");
+        }
+    }
+
+    /**
+     * 获取浮点型属性值
+     */
+    public float getFloatValue() throws MalformedFormatException {
+        try {
+            return Float.parseFloat(mValue);
+        }
+        catch (NumberFormatException e) {
+            throw new MalformedFormatException("should be decimal float");
+        }
+    }
+
+    /**
+     * 获取16进制序列型属性值
+     */
+    public byte[] getHexadecimalSequenceValue() throws MalformedFormatException {
+        return HexadecimalSequence.read(mValue);
+    }
+
+    /**
+     * 获取枚举字符串型属性值
+     */
+    public String getEnumeratedStringValue() {
+        return mValue;
+    }
+
+    /**
+     * 获取引用字符串型属性值
+     */
+    public String getQuotedStringValue() throws MalformedFormatException {
+        return QuotedString.read(mValue);
+    }
+
+    /**
+     * 获取分辨率型属性值
+     */
+    public Resolution getResolutionValue() throws MalformedFormatException {
+        return Resolution.valueOf(mValue);
+    }
+
+    @Override
+    public String toString() {
+        return mName + "=" + mValue;
+    }
+
+    /**
+     * 根据字符串创建属性
+     */
+    public static Attribute parse(String strAttribute) throws MalformedFormatException {
+        String[] result = strAttribute.split("=");
+        if (result.length != 2) {
+            throw new MalformedFormatException("should be name=value");
+        }
+
+        return new Attribute(result[0], result[1]);
+    }
+
+    /**
+     * 创建属性
+     */
+    public static Attribute create(String name, int value) {
+        return new Attribute(name, String.valueOf(value));
+    }
+
+    /**
+     * 创建属性
+     */
+    public static Attribute create(String name, long value) {
+        return new Attribute(name, String.valueOf(value));
+    }
+
+    /**
+     * 创建属性
+     */
+    public static Attribute create(String name, float value) {
+        return new Attribute(name, String.valueOf(value));
+    }
+
+    /**
+     * 创建属性
+     */
+    public static Attribute create(String name, byte[] value) {
+        return new Attribute(name, HexadecimalSequence.write(value));
+    }
+
+    /**
+     * 创建属性
+     */
+    public static Attribute create(String name, String value) {
+        return new Attribute(name, value);
+    }
+
+    /**
+     * 创建属性
+     */
+    public static Attribute create(String name, QuotedString value) {
+        return new Attribute(name, value.toString());
+    }
+
+    /**
+     * 创建属性
+     */
+    public static Attribute create(String name, Resolution value) {
+        return new Attribute(name, value.toString());
+    }
+
+    /**
+     * 属性名
+     */
+    public static class Name {
+        public static final String METHOD = "METHOD";
+        public static final String IV = "IV";
+        public static final String URI = "URI";
+        public static final String KEY_FORMAT = "KEYFORMAT";
+        public static final String KEY_FORMAT_VERSIONS = "KEYFORMATVERSIONS";
+        public static final String TYPE = "TYPE";
+        public static final String GROUP_ID = "GROUP-ID";
+        public static final String LANGUAGE = "LANGUAGE";
+        public static final String ASSOCIATED_LANGUAGE = "ASSOC-LANGUAGE";
+        public static final String NAME = "NAME";
+        public static final String DEFAULT = "DEFAULT";
+        public static final String AUTO_SELECT = "AUTOSELECT";
+        public static final String FORCED = "FORCED";
+        public static final String IN_STREAM_ID = "INSTREAM-ID";
+        public static final String CHARACTERISTICS = "CHARACTERISTICS";
+        public static final String CHANNELS = "CHANNELS";
+        public static final String BANDWIDTH = "BANDWIDTH";
+        public static final String AVG_BANDWIDTH = "AVERAGE-BANDWIDTH";
+        public static final String CODECS = "CODECS";
+        public static final String RESOLUTION = "RESOLUTION";
+        public static final String FRAME_RATE = "FRAME-RATE";
+        public static final String HDCP_LEVEL = "HDCP-LEVEL";
+        public static final String AUDIO = "AUDIO";
+        public static final String VIDEO = "VIDEO";
+        public static final String SUBTITLES = "SUBTITLES";
+        public static final String CLOSED_CAPTIONS = "CLOSED-CAPTIONS";
+        public static final String BYTE_RANGE = "BYTERANGE";
+
+        /**
+         * 构造函数（私有属性，不允许创建实例）
+         */
+        private Name() {
+            /**
+             * nothing
+             */
+        }
+    }
+}
--- app/src/main/java/com/iptv/core/hls/playlist/attribute/AttributeList.java
+++ app/src/main/java/com/iptv/core/hls/playlist/attribute/AttributeList.java
@@ -1,17 +1,14 @@
 package com.iptv.core.hls.playlist.attribute;
 
-import com.iptv.core.hls.playlist.ByteRange;
-import com.iptv.core.hls.playlist.datatype.Resolution;
-
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * 属性列表
  */
 public final class AttributeList {
-    private Map<String, Object> mTable = new HashMap<>();
+    private Map<Integer, Attribute> mTable = new HashMap<>();
 
     /**
      * 构造函数
@@ -39,165 +36,51 @@ public final class AttributeList {
     /**
      * 是否包含指定名称的属性
      */
-    public boolean containsName(String name) {
-        return mTable.containsKey(name);
+    public boolean containsAttribute(String name) {
+        int key = name.hashCode();
+        return mTable.containsKey(key);
     }
 
     /**
-     * 读（布尔型）属性值
+     * 获取指定名称的属性
      */
-    public Boolean getBoolean(String name) {
-        return (Boolean)get(name);
+    public Attribute get(String name) {
+        int key = name.hashCode();
+        return mTable.get(key);
     }
 
     /**
-     * 读（整型）属性值
+     * 放入属性
      */
-    public Integer getInteger(String name) {
-        return (Integer)get(name);
+    public void put(Attribute attribute) {
+        int key = attribute.getName().hashCode();
+        mTable.put(key, attribute);
     }
 
     /**
-     * 读（长整型）属性值
+     * 获取（属性）迭代器
      */
-    public Long getLong(String name) {
-        return (Long)get(name);
+    public Iterator<Attribute> iterator() {
+        return mTable.values().iterator();
     }
 
-    /**
-     * 读（字节数组类型）属性值
-     */
-    public byte[] getByteArray(String name) {
-        return (byte[])get(name);
-    }
+    @Override
+    public String toString() {
+        StringBuffer buffer = new StringBuffer();
 
-    /**
-     * 读（整型数组类型）属性值
-     */
-    public int[] getIntArray(String name) {
-        return (int[])get(name);
-    }
+        boolean isFirstAttribute = true;
+        for (Attribute attribute : mTable.values()) {
+            if (!isFirstAttribute) {
+                buffer.append(',');
+            }
 
-    /**
-     * 读（字符串数组类型）属性值
-     */
-    public String[] getStringArray(String name) {
-        return (String[])get(name);
-    }
+            buffer.append(attribute.toString());
 
-    /**
-     * 读（浮点型）属性值
-     */
-    public Float getFloat(String name) {
-        return (Float)get(name);
-    }
-
-    /**
-     * 读（字符串类型）属性值
-     */
-    public String getString(String name) {
-        return (String)get(name);
-    }
-
-    /**
-     * 读（分辨率类型）属性值
-     */
-    public Resolution getResolution(String name) {
-        return (Resolution)get(name);
-    }
-
-    /**
-     * 读（字节范围类型）属性值
-     */
-    public ByteRange getByteRange(String name) {
-        return (ByteRange)get(name);
-    }
-
-    /**
-     * 写（布尔型）属性值
-     */
-    public void putBoolean(String name, Boolean value) {
-        mTable.put(name, value);
-    }
-
-    /**
-     * 写（整型）属性值
-     */
-    public void putInteger(String name, Integer value) {
-        mTable.put(name, value);
-    }
-
-    /**
-     * 写（长整型）属性值
-     */
-    public void putLong(String name, Long value) {
-        mTable.put(name, value);
-    }
-
-    /**
-     * 写（字节数组类型）属性值
-     */
-    public void putByteArray(String name, byte[] value) {
-        mTable.put(name, value);
-    }
-
-    /**
-     * 写（整型数组类型）属性值
-     */
-    public void putIntArray(String name, int[] value) {
-        mTable.put(name, value);
-    }
-
-    /**
-     * 写（字符串数组类型）属性值
-     */
-    public void putStringArray(String name, String[] value) {
-        mTable.put(name, value);
-    }
-
-    /**
-     * 写（浮点型）属性值
-     */
-    public void putFloat(String name, Float value) {
-        mTable.put(name, value);
-    }
-
-    /**
-     * 写（字符串类型）属性值
-     */
-    public void putString(String name, String value) {
-        mTable.put(name, value);
-    }
-
-    /**
-     * 写（分辨率类型）属性值
-     */
-    public void putResolution(String name, Resolution value) {
-        mTable.put(name, value);
-    }
-
-    /**
-     * 写（字节范围类型）属性值
-     */
-    public void putByteRange(String name, ByteRange value) {
-        mTable.put(name, value);
-    }
-
-    /**
-     * 获取属性名集合
-     */
-    public Set<String> nameSet() {
-        return mTable.keySet();
-    }
-
-    /**
-     * 读属性值
-     */
-    public Object get(String name) {
-        if (!containsName(name)) {
-            throw new IllegalStateException("no " + name + " attribute");
+            if (isFirstAttribute) {
+                isFirstAttribute = false;
+            }
         }
 
-        return mTable.get(name);
+        return buffer.toString();
     }
 }
