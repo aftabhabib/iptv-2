8a74b2ee74f1ab155559fe02c550fad589043dc9
Hong Yu <jekyll.hong@hotmail.com>
2018-10-29 09:52:23 +0800

    Stream整合RenditionGroup中的getDefaultRendition和getRenditionByLanguage

--- app/src/main/java/com/iptv/core/hls/playlist/Stream.java
+++ app/src/main/java/com/iptv/core/hls/playlist/Stream.java
@@ -92,9 +92,35 @@ public final class Stream {
     /**
      * 获取（可替代的）音频展示
      */
-    private Rendition[] getAudioRenditions() {
+    public Rendition[] getAudioRenditions() {
         return mAudioRenditionList.toArray(
                 new Rendition[mAudioRenditionList.size()]);
+    }
+
+    /**
+     * 获取默认的音频展示
+     */
+    public Rendition getDefaultAudioRendition() {
+        for (Rendition rendition : mAudioRenditionList) {
+            if (rendition.isDefaultSelection()) {
+                return rendition;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * 获取与指定语言环境匹配的音频展示
+     */
+    public Rendition getAudioRenditionByLanguage(String language) {
+        for (Rendition rendition : mAudioRenditionList) {
+            if (rendition.getLanguage().equals(language)) {
+                return rendition;
+            }
+        }
+
+        return null;
     }
 
     /**
@@ -113,6 +139,19 @@ public final class Stream {
     }
 
     /**
+     * 获取默认的音频展示
+     */
+    public Rendition getDefaultVideoRendition() {
+        for (Rendition rendition : mVideoRenditionList) {
+            if (rendition.isDefaultSelection()) {
+                return rendition;
+            }
+        }
+
+        return null;
+    }
+
+    /**
      * 是否包含（可替代的）字幕展示
      */
     public boolean containsSubtitleRenditions() {
@@ -128,6 +167,32 @@ public final class Stream {
     }
 
     /**
+     * 获取默认的字幕展示
+     */
+    public Rendition getDefaultSubtitleRendition() {
+        for (Rendition rendition : mSubtitleRenditionList) {
+            if (rendition.isDefaultSelection()) {
+                return rendition;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * 获取与指定语言环境匹配的字幕展示
+     */
+    public Rendition getSubtitleRenditionByLanguage(String language) {
+        for (Rendition rendition : mSubtitleRenditionList) {
+            if (rendition.getLanguage().equals(language)) {
+                return rendition;
+            }
+        }
+
+        return null;
+    }
+
+    /**
      * 是否包含（可替代的）隱藏字幕展示
      */
     public boolean containsClosedCaptionRenditions() {
@@ -140,6 +205,32 @@ public final class Stream {
     public Rendition[] getClosedCaptionRenditions() {
         return mClosedCaptionRenditionList.toArray(
                 new Rendition[mClosedCaptionRenditionList.size()]);
+    }
+
+    /**
+     * 获取默认的隱藏字幕展示
+     */
+    public Rendition getDefaultClosedCaptionRendition() {
+        for (Rendition rendition : mClosedCaptionRenditionList) {
+            if (rendition.isDefaultSelection()) {
+                return rendition;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * 获取与指定语言环境匹配的隱藏字幕展示
+     */
+    public Rendition getClosedCaptionRenditionByLanguage(String language) {
+        for (Rendition rendition : mClosedCaptionRenditionList) {
+            if (rendition.getLanguage().equals(language)) {
+                return rendition;
+            }
+        }
+
+        return null;
     }
 
     /**
